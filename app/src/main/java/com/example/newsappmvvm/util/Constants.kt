36a3ddb0c93f364ad9ca09056b0c1b39114d6bf8
Junaed29 <junaed1995@gmail.com>
2021-03-06 13:58:08 +0600

    Retrofit Setup Complete

---
+++ app/src/main/java/com/example/newsappmvvm/util/Constants.kt
@@ -0,0 +1,9 @@
+package com.example.newsappmvvm.util
+
+class Constants {
+    companion object{
+        const val NEWS_API_KEY = "d68df4a5179a45ccb7be4072f1c2ce46"
+
+        const val NEWS_BASE_URL = "http://newsapi.org/"
+    }
+}
